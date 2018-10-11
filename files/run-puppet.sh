6ce154e4ba2aae74248bbd12c31c1b06ea89c4cc
Jim Vancheri <jvancher@comcast.net>
2018-10-11 18:06:05 +0000

    Add run-puppet script and cronjob

--- files/run-puppet.sh
+++ files/run-puppet.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd /etc/puppetlabs/code/environments/production && git pull
-/opt/puppetlabs/bin/puppet apply manifests/
+#/opt/puppetlabs/bin/puppet apply manifests/
+/opt/puppetlabs/puppet/bin/puppet apply manifests/
